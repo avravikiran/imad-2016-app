616960ae8cc1a1c3ed310b43209dc50e88fd4512
avravikiran <avenkataravikiran@gmail.com>
2016-09-19 01:48:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/ravi1.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ravi.png'));
+app.get('/ui/ravi-min.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ravi-min.png'));
 });
 
 app.get('/ui/fb.jpg', function (req, res) {
