831684ad1f1bcdead1a5e999f3e1a2d5c251e49a
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-04 00:22:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var Pool = require('pg').Pool;
 var path = require('path');
+
+
+var config = {
+  user: 'avravikiran',
+  host: 'db.imad.hasura-app.io',
+  password: process.env.DB_PASSWORD,
+  port: '5432',
+  database: 'avravikiran',
+};
 
 var app = express();
 app.use(morgan('combined'));
