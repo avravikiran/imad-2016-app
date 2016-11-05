66824e9818e9e4e4d1b1fe6e268110d2003bdc9d
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-06 04:36:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/ui/style.css', function (req, res) {
 var pool = new Pool(config);
 
 app.get('/ui/a', function (req, res) {
-    pool.query('INSERT INTO "user" ("user_name", "email","password") VALUES ("a","b","c")', function(err) {
+    pool.query('INSERT INTO user (email) VALUES ($1)', ['a'], function(err) {
         if(err)
         {
             res.end('An error occurred');
