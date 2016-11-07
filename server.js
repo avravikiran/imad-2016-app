296d4fb417ba07a358627dceb35455a8385b3df2
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-07 23:19:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var pool = new Pool(config);
 console.log(pool)
 
