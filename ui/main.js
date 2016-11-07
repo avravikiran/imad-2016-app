dee8593f5c812e7c9a4af5e92ec40b57af8dcea1
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-07 22:16:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,35 @@
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'avravikiran',
+  host: 'db.imad.hasura-app.io',
+  password: process.env.DB_PASSWORD,
+  port: '5432',
+  database: 'avravikiran'
+};
+
+
 console.log('Loaded!');
 document.getElementById("profile").onclick=function(){viewProfile()};
 
-function viewProfile()  {
-    getElementById("content").innerHTML="abcd"
+var pool = new Pool(config);
+
     
+app.get('/ui/a', function (req, res) {
+    pool.query("INSERT INTO \"user\" (\"user_name\", \"email\",\"password\") VALUES ('a','b','c')", function(err) {
+        if(err)
+        {
+            res.end('An error occurred');
+        }
+        else
+        {
+            res.writeHead(200, {'content-type': 'text/plain'});
+        }
+
+
+    });
+});
+
+function myFunction() {
+    document.getElementById("article").innerHTML = "Hello World";
 }
