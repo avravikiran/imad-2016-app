e6fc2a4c7bb062e563dbd91995141df4ffb9274c
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-07 23:41:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,4 @@
-var express = require('express');
-var morgan = require('morgan');
-var Pool = require('pg').Pool;
-var path = require('path');
 
-
-var config = {
-  user: 'avravikiran',
-  host: 'db.imad.hasura-app.io',
-  password: process.env.DB_PASSWORD,
-  port: '5432',
-  database: 'avravikiran'
-};
 
 console.log('Loaded!');
 
