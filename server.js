9cef44bdbc8a0ab4e50714ae09fb5a1bd9d963b8
A.V.Ravi kiran <me15btech11039@iith.ac.in>
2016-11-17 22:08:34 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.get('/abc', function (req, res) {
         if (err) {
             res.status(500).send(err.tostring());
         } else {
-            res.send(JSON.stringify(create(result.rows[10
+            res.send(JSON.stringify(create(result.rows[1])));
+        }
     });
 });
 
